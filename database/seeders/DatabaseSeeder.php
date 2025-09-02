276026b0f0ccb98342f2e998701a199f3b99ad34
Arkzado <jadiazosorio1@gmail.com>
2025-09-02 16:36:30 -0500

    utilización de factory con faker

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        User::factory()->create(['email'=>"admin@gmail.com"]);
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        User::factory(19)->hasPosts(4)->create();
     }
 }
